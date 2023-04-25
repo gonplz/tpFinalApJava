52d71a007832bab7ff990662103a10139f099e5b
gonplz <gonzaplazaola@gmai.com>
2023-04-25 18:12:56 -0300

    Iniciando Proyecto

---
+++ src/main/java/com/tpFinalAp/App.java
@@ -0,0 +1,13 @@
+package com.tpFinalAp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
