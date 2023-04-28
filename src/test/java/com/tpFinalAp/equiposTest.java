12ee0da196d7160607aa2226cca0b263f3a4ae8e
gonplz <gonzaplazaola@gmai.com>
2023-04-28 00:51:18 -0300

    Versión final del TPF Argentina Programa 4.0

---
+++ src/test/java/com/tpFinalAp/equiposTest.java
@@ -0,0 +1,12 @@
+package com.tpFinalAp;
+
+import junit.framework.TestCase;
+
+public class equiposTest extends App {
+
+    public void testConsultarTodosLosEquipos() {
+    }
+
+    public void testConsultarUnEquipo() {
+    }
+}
